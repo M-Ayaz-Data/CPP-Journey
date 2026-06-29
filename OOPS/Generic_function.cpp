6e3c1bb42431031cb06d52f1cd70d6d1179aefb6
Ayaz patel <ayazpatel353@gmail.com>
2026-06-29 22:26:51 +0530

    Add Generic function

---
+++ OOPS/Generic_function.cpp
@@ -0,0 +1,7 @@
+// template <typename T>
+// T add(T a, T b) {
+//     return a + b;
+// }
+
+
+// A generic function is a function that works with any data type (int, float, string, double, etc.) without rewriting the same logic for each type.
